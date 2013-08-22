3a0f97c8c9a0173e7955b872a451f961990add29
pablo.filetti@gmail.com <pablo.filetti@gmail.com@c8190d30-4896-7518-8053-1a57350f7098>
2013-08-22 05:03:23 +0000

    Atualização

--- jlibreoffice/src/test/java/com/googlecode/jlibreoffice/JLibreOfficeTest.java
+++ jlibreoffice/src/test/java/com/googlecode/jlibreoffice/JLibreOfficeTest.java
@@ -1,6 +1,8 @@
 package com.googlecode.jlibreoffice;
 
 import java.awt.BorderLayout;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.JFrame;
 
@@ -12,14 +14,38 @@ public class JLibreOfficeTest {
 	public static void main(String[] args) throws Exception {
 	
 		InstallationConfigs.iniciar();
-				
-		JFrame frame = new JFrame();
-		frame.setTitle("Teste");
-		frame.setSize(800, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		try {
-            JLibreOffice jLibreOffice = new JLibreOffice(InstallationConfigs.getInstance().getClassLoader());
+			final JLibreOffice jLibreOffice = new JLibreOffice(InstallationConfigs.getInstance().getClassLoader());
+			
+			JFrame frame = new JFrame();
+			frame.setTitle("Teste");
+			frame.setSize(800, 600);
+			frame.addWindowListener(new WindowListener() {
+				
+				public void windowOpened(WindowEvent e) {		
+				}
+				
+				public void windowIconified(WindowEvent e) {
+				}
+				
+				public void windowDeiconified(WindowEvent e) {
+				}
+				
+				public void windowDeactivated(WindowEvent e) {				
+				}
+				
+				public void windowClosing(WindowEvent e) {					
+				}
+				
+				public void windowClosed(WindowEvent e) {
+					jLibreOffice.closeConnection();
+					System.exit(0);
+				}
+				
+				public void windowActivated(WindowEvent e) {
+				}
+			});
             
             frame.add(jLibreOffice.getBean().getContainer(), BorderLayout.CENTER);            
             frame.setVisible(true);
